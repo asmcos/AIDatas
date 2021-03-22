cb2b5e5a013c374d67a84be9255aa8da0be57a2b
asmcos <asmcos@gmail.com>
2021-03-22 16:33:09 +0800

    增加函数存储所有的请求记录

--- api/request/controllers/request.js
+++ api/request/controllers/request.js
@@ -9,10 +9,46 @@ const { sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
   async create(ctx) {
-    let entity;
-      // entity = await strapi.services.post.create(ctx.request.body);
-    entity = await strapi.services.request.create(ctx.request.body);
+    let entity,responsedata,resp;
 
+    if (ctx.query.alldata == 1){
+        console.log(ctx.request.body)
+        entity = await strapi.services.request.create(ctx.request.body.request);
+        responsedata = ctx.request.body.response
+        responsedata.RequestID = entity.id
+
+        resp = await strapi.services.request.create(responsedata);
+
+        if (ctx.request.body.header != null ) {
+            ctx.request.body.header.map(async function(head){
+                if (head.type == 1) {
+                    head.parentid = entity.id + ""
+                } else {
+                    head.parentid = resp.id + ""
+                }
+                await strapi.services.header.create(head)
+            })
+
+        } //if
+
+        if (ctx.request.body.form != null ) {
+            ctx.request.body.form.map(async function(f){
+                if (f.type == 1) {
+                    f.parentid = entity.id + ""
+                } else {
+                    f.parentid = resp.id + ""
+                }
+                await strapi.services.form.create(f)
+            })
+        }
+
+
+
+    } else {
+
+        // entity = await strapi.services.post.create(ctx.request.body);
+        entity = await strapi.services.request.create(ctx.request.body);
+    }
    return sanitizeEntity(entity, { model: strapi.models.request });
   },
 };

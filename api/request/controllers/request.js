18bd9cd5669b1e3c144580fabeb6ab7c77e09f6e
asmcos <asmcos@gmail.com>
2021-03-23 11:20:29 +0800

    修改数据库

--- api/request/controllers/request.js
+++ api/request/controllers/request.js
@@ -9,34 +9,42 @@ const { sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
   async create(ctx) {
-    let entity,responsedata,resp;
+    let entity,responsedata,resp,host;
 
     if (ctx.query.alldata == 1){
-        console.log(ctx.request.body)
+
+        if (ctx.request.body.host == null) {
+            return "host verify failed"
+        }
+        host = await strapi.services.host.findOne({id:ctx.request.body.host.hostid,key:ctx.request.body.host.hostkey})
+        if (host == null) {
+
+            return "host verify failed"
+        }
+
+        ctx.request.body.request["host"]=host
         entity = await strapi.services.request.create(ctx.request.body.request);
         responsedata = ctx.request.body.response
-        responsedata.RequestID = entity.id
-
+        responsedata.request = entity
+        responsedata.host = host
         resp = await strapi.services.response.create(responsedata);
-
         if (ctx.request.body.header != null ) {
             ctx.request.body.header.map(async function(head){
                 if (head.type == 1) {
-                    head.parentid = entity.id + ""
+                    head.request = entity
                 } else {
-                    head.parentid = resp.id + ""
+                    head.response = resp
                 }
                 await strapi.services.header.create(head)
             })
 
         } //if
-
         if (ctx.request.body.form != null ) {
             ctx.request.body.form.map(async function(f){
                 if (f.type == 1) {
-                    f.parentid = entity.id + ""
+                    f.request = entity
                 } else {
-                    f.parentid = resp.id + ""
+                    f.response = resp
                 }
                 await strapi.services.form.create(f)
             })

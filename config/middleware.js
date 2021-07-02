4104cba0d34c8f04be7333f990240fac69b8fae5
jiasheng he <jiashenghe@jiashengtekiMacBook-Air.local>
2021-07-02 15:48:45 +0800

    修改大小限制post

--- config/middleware.js
+++ config/middleware.js
@@ -6,5 +6,13 @@ module.exports = {
     lib: {
       enabled: true,
     },
+    parser: {
+      // ...
+      formLimit: '10mb', // modify here limit of the form body
+      jsonLimit: '10mb', // modify here limit of the JSON body
+      formidable: {
+        maxFileSize: 100 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+      },
+    },
   },
 }

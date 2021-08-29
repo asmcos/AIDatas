5664f21a3abce078eb0dfb4ce47c9678fc66c850
asmcos <asmcos@gmail.com>
2021-08-29 18:20:42 +0800

    增加gushen 目录和静态路由

--- middlewares/lib/index.js
+++ middlewares/lib/index.js
@@ -1,11 +1,37 @@
 
-var path = require('path')
+var path = require('path');
+var staticdir = require('koa-static');
+var mount = require('koa-mount');
+var Router = require('koa-router');
+var swig = require('koa-swig');
+var co = require('co');
+
+var router = new Router();
 
 module.exports = strapi => {
   return {
     // can also be async
     initialize() {
-        strapi.app.use(require('koa-static')(path.resolve('./public/theme/Techie')))
+
+        strapi.app.context.render = co.wrap(
+            swig({
+                root:path.resolve('./public/theme/gushen'),
+                cache:false,
+                ext:'html'
+            })
+        )
+
+        router.get('/gushen/index.html',async(ctx,next)=>{
+            return ctx.body = await ctx.render("index_3.html")
+        })
+
+        strapi.app.use(router.routes())
+
+        strapi.app.use(staticdir(path.resolve('./public/theme/Techie')))
+        strapi.app.use(mount('/gushen',staticdir(path.resolve('./public/theme/gushen'))))
+
+
+
     }, // initialize
   }; // return
 };

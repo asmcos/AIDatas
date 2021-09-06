195221c6159efbd133bf3b5f743380024e1ceca1
jiasheng he <jiashenghe@jiashengtekiMacBook-Air.local>
2021-09-06 18:24:05 +0800

    增加事件记录

--- api/gs-categorystock/controllers/gs-categorystock.js
+++ api/gs-categorystock/controllers/gs-categorystock.js
@@ -13,18 +13,70 @@ const moment = require('moment')
 
 async function createGScatestock(ctx){
     let gs_catestock = strapi.models['gs-categorystock']
+    let gs_optevent = strapi.models['gs-optevent']
     var params = ctx.request.body
-
+    var params1
 
     if (!ctx.state.user){
         return "-1" //请先登录
     }
 
     params['users_permissions_user'] = ctx.state.user
-    params['buydate'] = moment(new Date()).format('YYYY-MM-DD')
-    return await gs_catestock.create(params)
+    params['startdate'] = moment(new Date()).format('YYYY-MM-DD')
+    var ret = await gs_catestock.create(params)
 
+    
+    params1['users_permissions_user'] = ctx.state.user
+    params1['code'] = params['code']
+    params1['name'] = params['name']
+    params1['count'] = params['count']
+    params1['categoryid'] = params['categoryid']
+    params1['price'] = params['buyprice']
+    params1['opttype'] = '1'
+
+    await gs_optevent.create(param1)
+
+    return ret
 }
+
+async function sellGScatestock(ctx){
+    let gs_catestock = strapi.models['gs-categorystock']
+    let gs_optevent = strapi.models['gs-optevent']
+    var params = ctx.request.body
+    var params1
+
+    if (!ctx.state.user){
+        return "-1" //请先登录
+    }
+
+
+    var ret = gs_catestock.findOne({'users_permissions_user':ctx.state.user,id:params['id']}) 
+    if(ret){
+        return "-2" //没有对应的持仓
+    }
+
+    if (ret.count - params['count'] < 0 ){
+
+        return "-3" //不够
+    }
+
+    await gs_catestock.updateOne({'users_permissions_user':ctx.state.user,id:params['id']},{'count': ret.count-params['count']})
+
+    
+    params1['users_permissions_user'] = ctx.state.user
+    params1['code'] = params['code']
+    params1['name'] = params['name']
+    params1['count'] = params['count']
+    params1['categoryid'] = params['categoryid']
+    params1['price'] = params['sellprice']
+    params1['opttype'] = '2'
+
+    await gs_optevent.create(param1)
+
+    return ret
+}
+
+
 
 
 
@@ -62,6 +114,10 @@ module.exports = {
     },
     async create(ctx){
         return createGScatestock(ctx)
+    },
+
+    async sell(ctx){
+        return sellGScatestock(ctx)
     },
 };
 
